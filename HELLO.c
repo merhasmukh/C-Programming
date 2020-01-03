e1c962a10e538083bcba44e84d5962d97249eeb0
mer hasmukh <55354471+mhgn@users.noreply.github.com>
2020-01-03 18:24:30 +0530

    Add files via upload
    print hello banner

---
+++ HELLO.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+
+
+printf("#    # ##### #     #      #####\n");
+printf("#    # #     #     #      #   #\n");
+printf("###### ###   #     #      #   #\n");
+printf("#    # #     #     #      #   #\n");
+printf("#    # ##### ####  #####  #####\n");
+
+
+}
